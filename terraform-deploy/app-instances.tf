4d643548c2c8cd713c00279ddd9f73005fb10ae6
jlyon1 <joeylyon1@gmail.com>
2018-03-14 14:55:57 -0400

    Separate redis and chat applications

--- terraform-deploy/app-instances.tf
+++ terraform-deploy/app-instances.tf
@@ -2,13 +2,43 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+resource "digitalocean_droplet" "redis" {
+  image              = "docker"
+  name               = "redis"
+  region             = "nyc3"
+  size               = "512mb"
+  private_networking = true
+  count              = 1
+
+  ssh_keys = [
+    "${var.ssh_fingerprint}",
+  ]
+
+  connection {
+    user        = "root"
+    type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout     = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d -p 6379:6379 redis:latest",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${digitalocean_droplet.redis.ipv4_address} >> private_ips.txt"
+  }
+}
+
 resource "digitalocean_droplet" "chat" {
   image              = "docker"
   name               = "chat"
   region             = "nyc3"
   size               = "512mb"
   private_networking = true
-  count = 5
+  count              = 2
 
   ssh_keys = [
     "${var.ssh_fingerprint}",
@@ -27,9 +57,8 @@ resource "digitalocean_droplet" "chat" {
       "cd terraformchat",
       "curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
       "chmod +x /usr/local/bin/docker-compose",
+      "export RP=${digitalocean_droplet.redis.ipv4_address}",
       "docker-compose up -d",
-
     ]
   }
-
 }
